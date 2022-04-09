93625b0e8826a572d0d36108107f8936227948ac
Micha Davis <m.leigh.davis@gmail.com>
2022-04-09 13:17:16 -0700

    add ssl config options for heroku

--- models/index.js
+++ models/index.js
@@ -8,7 +8,14 @@ const DATABASE_URL = process.env.NODE_ENV === 'test' ?
   'sqlite::memory' :
   process.env.DATABASE_URL || 'postgresql://localhost:5432/api-app';
 
-const sequelize = new Sequelize(DATABASE_URL);
+const sequelize = new Sequelize(DATABASE_URL, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
 const PeopleModel = peopleSchema(sequelize, DataTypes);
 const PlacesModel = placesSchema(sequelize, DataTypes);
 
